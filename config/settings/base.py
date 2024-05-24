c89a5c920333c78e853b153325cf037bed4977ba
Dipshina <dipshinatamang@gmail.com>
2024-05-24 16:46:05 +0545

    Project Structure Setup

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,6 +16,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+PROJECT_APPS =['apps.account', 'apps.commons', 'apps.main']
+
+THIRD_PARTY_APPS = []
+
+INSTALLED_APPS += PROJECT_APPS + THIRD_PARTY_APPS
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
